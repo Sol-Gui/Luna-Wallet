780a92fd564d088e2a6478dab2df001454366548
Gui.sol <int.guimarques@gmail.com>
2025-01-19 02:21:23 -0300

    fixed wallet bug

--- Backend/src/wallet.rs
+++ Backend/src/wallet.rs
@@ -1,9 +1,9 @@
-use solana_sdk::{signature::{Keypair}, pubkey::Pubkey};
+use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
 use bs58;
 
 pub fn create_new_wallet() {
     let keypair = Keypair::new();
-    let pubkey: Pubkey = keypair.pubkey();
+    let pubkey: Pubkey = keypair.try_pubkey().unwrap();
     let full_keypair_bytes = keypair.to_bytes();
 
     let full_private_key = bs58::encode(&full_keypair_bytes).into_string();
